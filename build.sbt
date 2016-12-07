4ecd68dea8e4da0a545b21739fe43d0cdef4d12b
katsuhide.tsuruta <katsuhide.tsuruta@gmail.com>
2016-12-07 16:47:44 +0900

    Consumer -> aerospike udf

--- build.sbt
+++ build.sbt
@@ -6,10 +6,12 @@ scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
   "org.scalaz" %% "scalaz-core" % "7.2.0",
-  "com.github.nscala-time" %% "nscala-time" % "2.8.0",
   "com.amazonaws" % "aws-java-sdk" % "1.11.52",
 //  "com.amazonaws" % "aws-java-sdk-core" % "1.11.51",
   "com.amazonaws" % "amazon-kinesis-client" % "1.7.1",
   "com.amazonaws" % "amazon-kinesis-producer" % "0.12.1",
-  "com.aerospike" % "aerospike-client" % "latest.integration"
+  "com.aerospike" % "aerospike-client" % "latest.integration",
+  "com.github.nscala-time" %% "nscala-time" % "2.8.0",
+  "org.json4s" %% "json4s-jackson" % "3.2.2",
+  "com.typesafe" % "config" % "1.3.1"
 )
---
+++ src/main/scala/models/SampleImpressionLog.scala
@@ -0,0 +1,8 @@
+package models
+
+/**
+  * Created by tsuruta on 2016/11/11.
+  */
+case class SampleImpressionLog(time: String, user: String, adId: Int) {
+  def toTsv(time: String): String = s"""{"application_id":0,"time":"$time","uuid":"$user","ad_id":$adId,"placement_id":2,"url":"//uresta.jp/オキテネムル","referer":"https://www.google.co.jp/","ua":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36","device":2,"lang":"ja,en-US;q=0.8,en;q=0.6","remote_ip_address":"","media_id":3,"article_id":7720,"priority":0.40650835985540756,"manual_priority":null,"similarity":0.5934916401445924,"manual_similarity":null,"default":0,"table_name":"article_impressions"}"""
+}
